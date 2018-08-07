9543574aa03fc53f11479ad8c93fb7cc5a758efa
ander7agar <ander7agar@gmail.com>
2018-08-07 12:48:49 +0200

    changin  configs

--- cmake-build-debug/build_version.cc
+++ cmake-build-debug/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
 const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@0@";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2018/07/17 09:26:11@";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2018/08/07 08:36:15@";
 const char* rocksdb_build_compile_date = __DATE__;
--- cmake-build-debug/libraries/utilities/git_revision.cpp
+++ cmake-build-debug/libraries/utilities/git_revision.cpp
@@ -1,9 +1,9 @@
 #include <stdint.h>
 #include <creativecoin/utilities/git_revision.hpp>
 
-#define CREA_GIT_REVISION_SHA "306e63b771295558e9ce3a1da476e708b5903c27"
-#define CREA_GIT_REVISION_UNIX_TIMESTAMP 1530651451
-#define CREA_GIT_REVISION_DESCRIPTION "v0.19.4rc1-561-g306e63b7"
+#define CREA_GIT_REVISION_SHA "90a8b18144a4e8d7ab8803b376de75ec4484fd01"
+#define CREA_GIT_REVISION_UNIX_TIMESTAMP 1533630975
+#define CREA_GIT_REVISION_DESCRIPTION "v0.19.4rc1-562-g90a8b181"
 
 namespace creativecoin { namespace utilities {
 
