167a57d3b5feea63d1d665a2f073194c436de0b8
ander7agar <ander7agar@gmail.com>
2018-08-07 13:23:57 +0200

    update ignore

--- cmake-build-debug/build_version.cc
+++
@@ -1,4 +0,0 @@
-#include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@0@";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2018/08/07 11:18:30@";
-const char* rocksdb_build_compile_date = __DATE__;
