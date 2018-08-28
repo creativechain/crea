89cc9ce23854fe50a1d755c368b3b74f98033c48
ander7agar <ander7agar@gmail.com>
2018-08-28 21:02:28 +0200

    update mining time

--- libraries/protocol/include/creativecoin/protocol/config.hpp
+++ libraries/protocol/include/creativecoin/protocol/config.hpp
@@ -51,7 +51,7 @@
 #define CREA_ADDRESS_PREFIX                  "CREA"
 
 #define CREA_GENESIS_TIME                    (fc::time_point_sec(1458835200))
-#define CREA_MINING_TIME                     (fc::time_point_sec(1458838800))
+#define CREA_MINING_TIME                     (fc::time_point_sec(1535482923))
 #define CREA_CASHOUT_WINDOW_SECONDS_PRE_HF12 (60*60*24)    /// 1 day
 #define CREA_CASHOUT_WINDOW_SECONDS_PRE_HF17 (60*60*12)    /// 12 hours
 #define CREA_CASHOUT_WINDOW_SECONDS          (60*60*24*7)  /// 7 days
