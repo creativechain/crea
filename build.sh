7959662ebaba5ae9889c0547a71907233299a0fd
ander7agar <ander7agar@gmail.com>
2018-09-11 12:40:41 +0200

    fixing compilation

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ SRC_DIR=$PWD
 BUILD_DIR=$SRC_DIR/build
 DEPENDS_DIR=$BUILD_DIR/depends
 DOWNLOAD_DIR=$DEPENDS_DIR/download
-INCLUDE_DIR=$DEPENDS_DIR/`uname -s`
+LIBRARIES_DIR=$DEPENDS_DIR/`uname -s`
 
 GCC_VERSION="5.4.0"
 
@@ -64,7 +64,7 @@ function buildBoost {
         fi
 
         cd $DOWNLOAD_DIR/boost_1_60_0
-        ./bootstrap.sh "--prefix=$INCLUDE_DIR"
+        ./bootstrap.sh "--prefix=$LIBRARIES_DIR"
         ./b2 install
         touch $file
      else
@@ -78,7 +78,7 @@ function buildOpenssl {
         downloadDependency "https://www.openssl.org/source/openssl-1.0.2o.tar.gz" "openssl-1.0.2o.tar.gz" "openssl-1.0.2o"
         printMsg "Building OpenSSL 1.0.2"
         cd $DOWNLOAD_DIR/openssl-1.0.2o
-        ./config --prefix=$INCLUDE_DIR
+        ./config --prefix=$LIBRARIES_DIR
         make
         make install
         touch $file
@@ -87,16 +87,108 @@ function buildOpenssl {
      fi
 }
 
+function buildZlib {
+    local file=$DEPENDS_DIR/.zlib
+    if [ ! -f $file ]; then
+        downloadDependency "https://zlib.net/zlib-1.2.11.tar.gz" "zlib-1.2.11.tar.gz" "zlib-1.2.11"
+        printMsg "Building ZLib 1.2.11"
+        cd $DOWNLOAD_DIR/zlib-1.2.11
+        make distclean
+        ./configure --prefix=$LIBRARIES_DIR
+        make
+        make install
+        touch $file
+     else
+        echo "ZLib already built..."
+     fi
+}
+
+function buildSnappy {
+    local file=$DEPENDS_DIR/.snappy
+    if [ ! -f $file ]; then
+        downloadDependency "https://github.com/google/snappy/archive/1.1.7.tar.gz" "snappy-1.1.7.tar.gz" "snappy-1.1.7"
+        printMsg "Building Snappy 1.1.7"
+        cd $DOWNLOAD_DIR/snappy-1.1.7
+        cmake -DCMAKE_INSTALL_PREFIX=$LIBRARIES_DIR
+        make all install
+        touch $file
+     else
+        echo "Snappy already built..."
+     fi
+}
+
+function buildNCurses {
+    local file=$DEPENDS_DIR/.ncurses
+    if [ ! -f $file ]; then
+        downloadDependency "https://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.1.tar.gz" "ncurses-6.1.tar.gz" "ncurses-6.1"
+        printMsg "Building NCurses 6.1"
+        cd $DOWNLOAD_DIR/ncurses-6.1
+        ./configure --prefix=$LIBRARIES_DIR
+        make
+        make install
+        touch $file
+     else
+        echo "Ncurses already built..."
+     fi
+}
+
+function buildReadline {
+
+    local file=$DEPENDS_DIR/.readline
+    if [ ! -f $file ]; then
+        downloadDependency "http://git.savannah.gnu.org/cgit/readline.git/snapshot/readline-6.3.tar.gz" "readline-6.3.tar.gz" "readline-6.3"
+        printMsg "Building Readline 6.3"
+        cd $DOWNLOAD_DIR/readline-6.3
+        ./configure --prefix=$LIBRARIES_DIR
+        make
+        make install
+        touch $file
+     else
+        echo "Readline already built..."
+     fi
+}
+
+function buildBZip2 {
+
+    local file=$DEPENDS_DIR/.bzip2
+    if [ ! -f $file ]; then
+        downloadDependency "https://api.github.com/repos/enthought/bzip2-1.0.6/tarball/master" "bzip2-1.0.6.tar.gz" "bzip2-1.0.6"
+        mkdir $DOWNLOAD_DIR/bzip2-1.0.6
+        mv $DOWNLOAD_DIR/enthought-bzip2-1.0.6-288acf9/* $DOWNLOAD_DIR/bzip2-1.0.6/
+        printMsg "Building BZip2 1.0.6"
+        cd $DOWNLOAD_DIR/bzip2-1.0.6
+        make install PREFIX=$LIBRARIES_DIR
+        touch $file
+     else
+        echo "BZip2 already built..."
+     fi
+
+}
+
 function buildSource {
+    buildBoost
+    buildOpenssl
+    buildZlib
+    buildSnappy
+    buildNCurses
+    buildReadline
+    buildBZip2
+
     printMsg "Building source"
     cd $BUILD_DIR
-    export BOOST_ROOT=$INCLUDE_DIR
-    export OPENSSL_ROOT_DIR=$INCLUDE_DIR
-    if [ ! checkGccVersion ]; then
-        cmake -DCMAKE_CXX_FLAGS_RELEASE="-fpermissive -O3" -DENABLE_CONTENT_PATCHING=OFF -DLOW_MEMORY_NODE=ON -DCMAKE_BUILD_TYPE=Release ..
-    else
-        cmake -DCMAKE_CXX_FLAGS_RELEASE="-fpermissive -O3" -DCMAKE_BUILD_TYPE=Release ..
-    fi
+
+    export BOOST_ROOT=$LIBRARIES_DIR
+    export OPENSSL_ROOT_DIR=$LIBRARIES_DIR
+    export ZLIB_ROOT=$LIBRARIES_DIR
+    export SNAPPY_LIBRARIES=$LIBRARIES_DIR/lib
+    export SNAPPY_INCLUDE_DIR=$LIBRARIES_DIR/include
+    export CURSES_ROOT=$LIBRARIES_DIR
+    export READLINE_ROOT=$LIBRARIES_DIR
+    export BZIP2_ROOT=$LIBRARIES_DIR
+
+    cmake -DCMAKE_CXX_FLAGS_RELEASE="-fpermissive -O3" -DENABLE_CONTENT_PATCHING=OFF -DLOW_MEMORY_NODE=ON -DSKIP_BY_TX_ID=ON -DCMAKE_BUILD_TYPE=Release ..
+
+    export PERL_USE_UNSAFE_INC=1
 
     make -j$(nproc) creativecoind
     make -j$(nproc) cli_wallet
@@ -104,6 +196,5 @@ function buildSource {
 
 makeDir $DOWNLOAD_DIR
 git submodule update --init --recursive
-buildBoost
-buildOpenssl
+
 buildSource
