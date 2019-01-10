e895be921e637153c4afdb17f99bba54423ccc2b
ander7agar <ander7agar@gmail.com>
2019-01-10 20:36:23 +0100

    fixed hourly

--- libraries/protocol/include/crea/protocol/config.hpp
+++ libraries/protocol/include/crea/protocol/config.hpp
@@ -60,7 +60,7 @@
 #define CREA_MINING_TIME                     (fc::time_point_sec(1546463767))
 #define CREA_CASHOUT_WINDOW_SECONDS_PRE_HF12 (60*60*24)    /// 1 day
 #define CREA_CASHOUT_WINDOW_SECONDS_PRE_HF17 (60*60*12)    /// 12 hours
-#define CREA_CASHOUT_WINDOW_SECONDS          (60*60)  /// 7 days -> Rewards
+#define CREA_CASHOUT_WINDOW_SECONDS          (60*60*24*7)  /// 7 days -> Rewards
 #define CREA_SECOND_CASHOUT_WINDOW           (60*60*24*30) /// 30 days
 #define CREA_MAX_CASHOUT_WINDOW_SECONDS      (60*60*24*14) /// 2 weeks
 #define CREA_UPVOTE_LOCKOUT_HF7              (fc::minutes(1))
