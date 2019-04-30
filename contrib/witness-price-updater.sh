f018d5b72265d2f3ec980bb510d007ba43e93000
ander7agar <ander7agar@gmail.com>
2019-04-30 11:25:01 +0200

    fix node for updater feed script

--- contrib/witness-price-updater.sh
+++ contrib/witness-price-updater.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-crea-witness update-price $CREAD_PRIVATE_KEY $CREAD_WITNESS_NAME
+if [[ ! -z "$HTTP_PORT" ]]; then
+    HTTP_PORT=$HTTP_PORT
+else
+    HTTP_PORT=1996
+fi
+crea-witness update-price --node http://lcoalhost:$HTTP_PORT $CREAD_PRIVATE_KEY $CREAD_WITNESS_NAME
