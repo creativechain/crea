a1089b030aed12821df10603e43cd658ee0a9a6e
ander7agar <ander7agar@gmail.com>
2019-04-30 11:57:02 +0200

    fix node for updater feed script

--- contrib/witness-price-updater.sh
+++ contrib/witness-price-updater.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+if [[ ! -z "$NODE_UPDATER" ]]; then
+    NODE_UPDATER=$NODE_UPDATER
+else
+    NODE_UPDATER=https://nodes.creary.net
+fi
+
 crea-witness update-price --node $NODE_UPDATER $CREAD_PRIVATE_KEY $CREAD_WITNESS_NAME
