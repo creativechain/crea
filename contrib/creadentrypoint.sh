1a9dbcb74cb205607563ca4050005dea7ee97fe6
ander7agar <ander7agar@gmail.com>
2019-05-01 23:57:30 +0200

    fixing feed price execution

--- contrib/creadentrypoint.sh
+++ contrib/creadentrypoint.sh
@@ -3,18 +3,7 @@
 echo /tmp/core | tee /proc/sys/kernel/core_pattern
 ulimit -c unlimited
 
-# if we're not using PaaS mode then start cread traditionally with sv to control it
-if [[ ! "$USE_PAAS" ]]; then
-  mkdir -p /etc/service/cread
-  cp /usr/local/bin/crea-sv-run.sh /etc/service/cread/run
-  chmod +x /etc/service/cread/run
-  runsv /etc/service/cread
-elif [[ "$IS_TESTNET" ]]; then
-  /usr/local/bin/pulltestnetscripts.sh
-else
-  /usr/local/bin/startpaascread.sh
-fi
-
+# Enable price updater
 cp /etc/cread/witness-price-updater.sh /usr/local/bin/witness-price-updater
 
 if [[ ! -z "$NODE_UPDATER" ]]; then
@@ -29,3 +18,16 @@ sed -i "s|WITNESS=|WITNESS=${CREAD_WITNESS_NAME}|g" /usr/local/bin/witness-price
 sed -i "s|PRIVATE_KEY=|PRIVATE_KEY=${CREAD_PRIVATE_KEY}|g" /usr/local/bin/witness-price-updater
 
 chmod +x /usr/local/bin/witness-price-updater
+
+# if we're not using PaaS mode then start cread traditionally with sv to control it
+if [[ ! "$USE_PAAS" ]]; then
+  mkdir -p /etc/service/cread
+  cp /usr/local/bin/crea-sv-run.sh /etc/service/cread/run
+  chmod +x /etc/service/cread/run
+  runsv /etc/service/cread
+elif [[ "$IS_TESTNET" ]]; then
+  /usr/local/bin/pulltestnetscripts.sh
+else
+  /usr/local/bin/startpaascread.sh
+fi
+
